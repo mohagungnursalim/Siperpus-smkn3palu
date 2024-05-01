745e951bafd7a6e3eff5a156fcab03a0e0eb6575
mohagungnursalim <mohagungnursalim@gmail.com>
2024-05-01 12:14:20 +0800

    feat: menambahkan get,store,update,delete pada KategoriController

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\kategori;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
@@ -10,9 +10,15 @@ class KategoriController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $kategories = Kategori::oldest()->paginate(10)->WithQueryString();
+
+        if (request('search')) {
+            $kategories = Kategori::where('nama_kategori', 'like' , '%' . request('search') . '%' )->oldest()->paginate(10)->WithQueryString();
+        }
+
+        return view('dashboard.kategori.index',compact('kategories'));
     }
 
     /**
@@ -28,7 +34,18 @@ class KategoriController extends Controller
      */
     public function store(Request $request)
     {
-        //
+           // Validasi input yang diterima dari form
+           $request->validate([
+            'nama_kategori' => 'required|string|max:25',
+        ]);
+    
+        // Jika tidak ada kesalahan validasi, buat dan simpan data anggota ke dalam database
+        Kategori::create([
+            'nama_kategori' => $request->nama_kategori,
+
+        ]);
+    
+        return redirect('/dashboard/kategori')->with('success', 'Kategori berhasil ditambahkan!');
     }
 
     /**
@@ -50,16 +67,34 @@ class KategoriController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, kategori $kategori)
+    public function update(Request $request, kategori $kategori, $id)
     {
-        //
+        // mencari kategori berdasarkan id
+        $kategori = Kategori::findOrFail($id);
+
+          // Validasi input yang diterima dari form
+          $request->validate([
+            'nama_kategori' => 'required|string|max:25',
+        ]);
+    
+        // Jika tidak ada kesalahan validasi, buat dan simpan data kategori ke dalam database
+        $kategori->update([
+            'nama_kategori' => $request->nama_kategori,
+
+        ]);
+    
+        return redirect('/dashboard/kategori')->with('success', 'Kategori berhasil diperbarui!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(kategori $kategori)
+    public function destroy(kategori $kategori,$id)
     {
-        //
+        $kategori = Kategori::findOrFail($id);
+
+        $kategori->delete();
+
+        return redirect('/dashboard/kategori')->with('success', 'Kategori berhasil dihapus!');
     }
 }
