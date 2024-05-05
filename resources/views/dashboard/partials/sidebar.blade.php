1faf4c4cfd0c973bbf3494c989ece7a7e976b7bb
mohagungnursalim <mohagungnursalim@gmail.com>
2024-05-05 09:23:10 +0800

    feat: menambahkan profile dan logout di sidebar

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -1,96 +1,151 @@
 <aside
-        class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
-        id="sidenav-main">
+    class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
+    id="sidenav-main">
 
-        <div class="sidenav-header">
-            <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-                aria-hidden="true" id="iconSidenav"></i>
-            <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
-                target="_blank">
-                <img src="{{asset('assets/img/logo-ct.png')}}" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold text-white">Material Dashboard 2</span>
-            </a>
-        </div>
+    <div class="sidenav-header">
+        <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+            aria-hidden="true" id="iconSidenav"></i>
+        <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
+            target="_blank">
+            <img src="{{asset('assets/img/logo-ct.png')}}" class="navbar-brand-img h-100" alt="main_logo">
+            <span class="ms-1 font-weight-bold text-white">Material Dashboard 2</span>
+        </a>
+    </div>
 
 
-        <hr class="horizontal light mt-0 mb-2">
+    <hr class="horizontal light mt-0 mb-2">
 
-        <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link text-white {{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link text-white {{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard">
 
-                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                           {{-- icon --}}
-                           <span class="material-symbols-outlined">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        {{-- icon --}}
+                        <span class="material-symbols-outlined">
                             bar_chart_4_bars
-                           </span>
-                        </div>
+                        </span>
+                    </div>
 
-                        <span class="nav-link-text ms-1">Dashboard</span>
-                    </a>
-                </li>
+                    <span class="nav-link-text ms-1">Dashboard</span>
+                </a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="/dashboard/anggota">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="/dashboard/anggota">
 
-                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                           {{-- icon --}}
-                           
-                           <span class="material-symbols-outlined">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        {{-- icon --}}
+
+                        <span class="material-symbols-outlined">
                             cycle
-                            </span>
-                        </div>
+                        </span>
+                    </div>
 
-                        <span class="nav-link-text ms-1">Peminjaman</span>                    
-                    </a>
-                </li>
-                
-                <li class="nav-item">
-                    <a class="nav-link text-white {{ Request::is('dashboard/anggota') ? 'active' : '' }} " href="/dashboard/anggota">
+                    <span class="nav-link-text ms-1">Peminjaman</span>
+                </a>
+            </li>
 
-                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                           {{-- icon --}}
-                           
-                           <span class="material-symbols-outlined">
+            <li class="nav-item">
+                <a class="nav-link text-white {{ Request::is('dashboard/anggota') ? 'active' : '' }} "
+                    href="/dashboard/anggota">
+
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        {{-- icon --}}
+
+                        <span class="material-symbols-outlined">
                             group
-                            </span>
-                        </div>
+                        </span>
+                    </div>
 
-                        <span class="nav-link-text ms-1">Anggota</span>                    
-                    </a>
-                </li>
+                    <span class="nav-link-text ms-1">Anggota</span>
+                </a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link text-white {{Request::is('dashboard/buku') ? 'active' : ''}} " href="/dashboard/buku">
+            <li class="nav-item">
+                <a class="nav-link text-white {{Request::is('dashboard/buku') ? 'active' : ''}} "
+                    href="/dashboard/buku">
 
-                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                           {{-- icon --}}
-                           
-                           <span class="material-symbols-outlined">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        {{-- icon --}}
+
+                        <span class="material-symbols-outlined">
                             library_books
-                            </span>
-                        </div>
+                        </span>
+                    </div>
 
-                        <span class="nav-link-text ms-1">Buku</span>                    
-                    </a>
-                </li>
+                    <span class="nav-link-text ms-1">Buku</span>
+                </a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link text-white {{ Request::is('dashboard/kategori') ? 'active' : '' }}" href="/dashboard/kategori">
+            <li class="nav-item">
+                <a class="nav-link text-white {{ Request::is('dashboard/kategori') ? 'active' : '' }}"
+                    href="/dashboard/kategori">
 
-                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                           {{-- icon --}}
-                           
-                           <span class="material-symbols-outlined">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        {{-- icon --}}
+
+                        <span class="material-symbols-outlined">
                             list
-                            </span>
-                        </div>
+                        </span>
+                    </div>
 
-                        <span class="nav-link-text ms-1">Kategori</span>                    
-                    </a>
-                </li>
-                
-            </ul>
-        </div>
-    </aside>
+                    <span class="nav-link-text ms-1">Kategori</span>
+                </a>
+            </li>
+
+            <li class="nav-item mt-3">
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">
+                    ----------------Akun-------------------
+                </h6>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-white " href="../pages/profile.html">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">person</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Profile</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-white" data-bs-toggle="modal" data-bs-target="#logoutModal">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">logout</i>
+                    </div>
+                    <span class="nav-link-text ms-1">LogOut</span>
+                </a>
+            </li>
+
+
+
+            <!-- Modal -->
+            <div class="modal" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title font-weight-normal" id="exampleModalLabel">LogOut Confirmation</h5>
+                            <button type="button" class="btn-close text-dark" data-bs-dismiss="modal"
+                                aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <h4>Dengan mengklik LogOut maka Anda keluar dari akun Anda!</h3>
+                        </div>
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <div class="modal-footer">
+                                <button type="button" class="btn bg-gradient-secondary"
+                                    data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn bg-gradient-primary">LogOut</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </ul>
+    </div>
+</aside>
