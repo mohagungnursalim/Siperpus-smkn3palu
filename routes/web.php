543cbf013d2df5de61136c42039b7839299b3fbb
mohagungnursalim <mohagungnursalim@gmail.com>
2024-05-05 16:54:39 +0800

    feat: change route profile menjadi /dashboard/profile

--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,9 @@ Route::resource('/dashboard/buku', BukuController::class)->middleware('auth');
 Route::resource('/dashboard/user', UserController::class)->middleware('auth');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/dashboard/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/dashboard/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
