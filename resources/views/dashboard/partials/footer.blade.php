a02d107c0dd6209c35681e325c81a5b21f95456d
mohagungnursalim <mohagungnursalim@gmail.com>
2024-05-12 02:14:30 +0800

    fix: mengubah tampilan footer

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -6,11 +6,8 @@
                     © <script>
                         document.write(new Date().getFullYear())
 
-                    </script>,
-                    made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative
-                        Tim</a>
-                    for a better web.
+                    </script>
+                    SMK Negeri 3 Palu
                 </div>
             </div>
         </div>
